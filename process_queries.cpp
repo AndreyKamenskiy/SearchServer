92cffd6fe4feef6921c2f42e670887c600c48151
AndreyKamenskiy <rybinsk@mail.ru>
2021-04-12 19:47:18 +0300

    add ProcessQueriesJoined(

--- process_queries.cpp
+++ process_queries.cpp
@@ -17,3 +17,28 @@ std::vector<std::vector<Document>> ProcessQueries(
     );
     return res;
 }
+
+std::vector<Document> ProcessQueriesJoined(
+    const SearchServer& search_server,
+    const std::vector<std::string>& queries) {
+    std::vector<std::vector<Document>> res(queries.size());
+    size_t size = 0;
+
+    std::transform(
+        std::execution::par,
+        queries.begin(), queries.end(),
+        res.begin(),
+        [&search_server, &size](const std::string& query) {
+            std::vector<Document> queryRes = search_server.FindTopDocuments(query);
+            size += queryRes.size();
+            return queryRes;
+        }
+    );
+    std::vector<Document> joined(size);
+    auto firstEmpty = joined.begin();
+    for (auto& currRes : res) {
+        firstEmpty = std::move(currRes.begin(), currRes.end(), firstEmpty);
+    }
+
+    return joined;
+}
